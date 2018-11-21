bc13a1d6850779c0116ae18e8e9ea0903d6abe38
Bikmay <artyom.ivliev@gmail.com>
2018-11-22 02:22:32 +0400

    Initial commit

---
+++ src/main/java/models/Hero.java
@@ -0,0 +1,22 @@
+package models;
+
+import java.io.File;
+
+public class Hero {
+
+    public int id;
+    public String name;
+    public Universe universe;
+    public int power;
+
+    public String description;
+    public boolean alive;
+
+    public File logo;
+    public int phone;
+
+
+
+
+
+}
